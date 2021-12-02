8bb28f83a207bae7052af2b8467fa8f52133912a
joshmeranda <joshmeranda@gmail.com>
2021-12-01 21:09:46 -0500

    basic /games route

--- client-js/src/index.tsx
+++ client-js/src/index.tsx
@@ -1,52 +1,34 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import { MinesweeperService } from './api/api'
-import { GameData } from './api/types';
-import { GameComponent } from './components/game';
+import { App } from './App';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import '98.css';
 import './components/minesweeper/cell.css';
 import './components/minesweeper/minefield.css';
-
-interface IProps {
-    service: MinesweeperService,    
-}
-
-interface IState {
-    game?: GameData,
-}
-
-/**
- * Basic wrapper component around the game component which fetches the game data.
- */
-class WrapperComponent extends React.Component<IProps, IState> {
-    constructor(props: IProps) {
-        super(props);
-
-        this.state = {
-        
-        };
-    }
-
-    componentDidMount() {
-        if (this.state.game === undefined) {
-            this.props.service.createGame(10, 10, 4)
-            .then(id => this.props.service.getGame(id)
-                .then(data => this.setState({game: data})));
-        }
-    }
-
-    render() {
-        return <div>{ this.state.game !== undefined ? <GameComponent service={this.props.service} gameData={this.state.game} /> : 'no game data available'}</div>
-    }
-}
+import { GamesRoute } from './routes/games';
 
 let service = new MinesweeperService("localhost:5000");
 
+service.createGame(10, 10, 4);
+service.createGame(10, 10, 10);
+
+async function sleep(ms: number) {
+    await new Promise(resolve => setTimeout(resolve, ms));
+}
+
+sleep(2000);
+
+const rootElement  = document.getElementById("root");
+
 // todo: add routes
 ReactDOM.render(
-  <React.StrictMode>
-    <WrapperComponent service={service}/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App service={service}/>} />
+            <Route path="/games" element={<GamesRoute service={service} />} />
+            {/* <Route path="game/:id" /> */}
+        </Routes>
+    </BrowserRouter>,
+    rootElement
 );
---
+++ client-js/src/routes/games.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { MinesweeperService } from '../api/api';
+import { GameData } from '../api/types';
+
+interface GamesRouteProps {
+    service: MinesweeperService,
+}
+
+export function GamesRoute(props: GamesRouteProps): JSX.Element {
+    let [searchParams, _] = useSearchParams();
+    let page = searchParams.get("page");
+    let size = searchParams.get("size");
+
+    let [error, setError] = useState<string | undefined>(undefined);
+    let [games, setGames] = useState<GameData[]>([]);
+    let [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+    useEffect(() => {
+        props.service.getGames(
+            page !== null ? parseInt(page) : undefined,
+            size !== null ? parseInt(size) : undefined)
+                .then(
+                    page => {
+                        setGames(page.data);
+                        setIsLoaded(true);
+                    },
+                    error => {
+                        setError(error);
+                        setIsLoaded(error);
+                    }
+                )
+    }, []);
+
+    if (error !== undefined) {
+        return <p>ERROR: {error.toString()}</p>
+    } else if (! isLoaded) {
+        return <p>loading...</p>
+    } else if (games.length === 0) {
+        return <p>no games were found mathing the given criteria</p>
+    } else {
+        return <ul>
+            {
+                games.map((game: GameData, i:number) => <li key={i}>{JSON.stringify(game)}</li>)
+            }
+        </ul>
+    }
+}
